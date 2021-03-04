c98d5dda2d38cb07e5204cdc8af58e0b9458705e
Toyin Dawudu <toyindawudu@gmail.com>
2021-03-04 23:49:09 +0000

    Finished working on the header

--- js/main.js
+++ js/main.js
@@ -23,14 +23,41 @@ showMenu('nav-toggle','nav-menu')
 const navLink = document.querySelectorAll('.nav__link');
 
 function linkAction(){
-  const navMenu = document.getElementById('nav-menu')
+  const navMenu = document.getElementById('nav-menu');
   // When we click on the nav__link, we want to remove the show-menu class from navMenu
-  navMenu.classList.remove('show-menu')
+  navMenu.classList.remove('show-menu');
 }
 
 navLink.forEach(item => item.addEventListener('click', linkAction));  
 
 
 /*=========================
+    BACKGROUND HEADER
+========================== */
+
+function scrollHeader(){
+  const nav = document.getElementById('header');
+  // When the scroll is greater than 200 viewport height, add the scroll header class to the element
+  if(this.scrollY >= 200) {
+    nav.classList.add('scroll-header');
+  } else {
+    nav.classList.remove('scroll-header');
+  }
+}
+window.addEventListener('scroll', scrollHeader);
+
+/*=========================
     SHOW SCROLL TO THE TOP
 ========================== */
+
+function scrollTop(){
+  const scrollTop = document.getElementById('scroll-top');
+  // When the scroll is greater than 560 viewport height, add the scroll header class to the element
+  if(this.scrollY >= 560) {
+    scrollTop.classList.add('scroll-top');
+  } else {
+    scrollTop.classList.remove('scroll-top');
+  }
+}
+window.addEventListener('scroll', scrollTop);
+
