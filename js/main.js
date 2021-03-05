d35b80840f82a29c19fd6e51421a8755e0fc86a5
Toyin Dawudu <toyindawudu@gmail.com>
2021-03-05 17:12:02 +0000

    I'm trying to solve the mobile layout for my landing page

--- js/main.js
+++ js/main.js
@@ -30,6 +30,29 @@ function linkAction(){
 
 navLink.forEach(item => item.addEventListener('click', linkAction));  
 
+/*=========================
+    ADD ACTIVE LINK
+========================== */
+ const sections = document.querySelectorAll('section[id');
+
+ window.addEventListener('scroll', scrollActive);
+
+ function scrollActive(){
+   const scrollY = window.pageYOffset;
+
+   sections.forEach(current => {
+     const sectionHeight = current.offsetHeight;
+     const sectionTop = current.offsetTop - 50;
+     sectionId = current.getAttribute('id')
+
+     if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+        document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+     } else {
+      document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+     }
+   })
+
+ }
 
 /*=========================
     BACKGROUND HEADER
